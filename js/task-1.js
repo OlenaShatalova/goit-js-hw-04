328753df0fbe85f50458ccecf320eda5ebb16833
Olena Shatalova <164022674+OlenaShatalova@users.noreply.github.com>
2024-06-20 15:45:18 +0300

    add-task-1,2

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+function isEnoughCapacity(products, containerSize) {
+  let productCapacity = 0;
+  const allValueArr = Object.values(products);
+
+  for (const value of allValueArr) {
+    productCapacity += value;
+  }
+
+  return productCapacity <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
